082529d21ede51ad85b6b9652b193eb809c8c2a2
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-28 19:01:58 +0530

    Stage 2

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,2 +1,7 @@
 # write your code here
-print(f"{(int(input()) + int(input()) + int(input())) / 3}\nCongratulations, you are accepted!")
+mean = (int(input()) + int(input()) + int(input())) / 3
+print(f"{mean}")
+if mean >= 60.0:
+    print("Congratulations, you are accepted!")
+else:
+    print("We regret to inform you that we will not be able to offer you admission.")
