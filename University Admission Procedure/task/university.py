7044e092e88b2631acc4f54a9452675f23deb7ff
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-03-30 23:39:25 +0530

    Stage 3

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -3,6 +3,9 @@ M = int(input())
 applicants = []
 for _ in range(N):
     first_name, last_name, gpa = input().split(" ")
-    applicants.append([first_name, last_name, float(gpa)])
+    applicants.append([first_name + " " + last_name, float(gpa)])
 
-print()
+print("Successful applicants:")
+success_applicants = sorted(applicants, key=lambda x: (-x[1], x[0]))
+for applicant in success_applicants:
+    print(applicant[0])
