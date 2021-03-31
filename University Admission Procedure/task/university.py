906bddb08a7be28b4b0ffd621475a929d76cb8e6
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-04-01 00:41:16 +0530

    Stage 4

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,9 +1,10 @@
 N = int(input())
 
 applicants = []
+final_list = {"Biotech": [], "Chemistry": [], "Engineering": [], "Mathematics": [], "Physics": []}
 with open("applicants.txt", 'r') as data:
     for line in data:
-        first_name, last_name, gpa, ch1, ch2, ch3 = data.readline().split(" ")
+        first_name, last_name, gpa, ch1, ch2, ch3 = data.readline().strip().split(" ")
         applicants.append([first_name + " " + last_name, float(gpa), ch1, ch2, ch3])
-    sorted_applicants = sorted(applicants, key=lambda x: (-x[1], x[0]))
-
+    sorted_applicants = sorted(applicants, key=lambda x: (x[2], -x[1], x[3], x[4], x[0]))
+    for
