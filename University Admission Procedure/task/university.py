01f995b2ebb66f62207307a9a45bd50b25ddbd7f
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-04-01 02:14:27 +0530

    Stage 4

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -4,9 +4,9 @@ applicants = []
 final_list = {"Biotech": [], "Chemistry": [], "Engineering": [], "Mathematics": [], "Physics": []}
 with open("applicants.txt", 'r') as data:
     for line in data:
-        first_name, last_name, gpa, ch1, ch2, ch3 = data.readline().strip().split(" ")
+        first_name, last_name, gpa, ch1, ch2, ch3 = line.strip().split(" ")
         applicants.append([first_name + " " + last_name, float(gpa), ch1, ch2, ch3])
-    sorted_applicants = sorted(applicants, key=lambda x: (-x[1], x[0]))
+    # sorted_applicants = sorted(applicants, key=lambda x: (-x[1], x[0]))
     first_priority = sorted(applicants, key=lambda x: (x[2], -x[1], x[0]))
     second_priority = sorted(applicants, key=lambda x: (x[3], -x[1], x[0]))
     third_priority = sorted(applicants, key=lambda x: (x[4], -x[1], x[0]))
@@ -18,11 +18,13 @@ with open("applicants.txt", 'r') as data:
     for applicant in second_priority:
         if len(final_list[applicant[3]]) < N:
             final_list[applicant[3]].append(applicant)
-            second_priority.remove(applicant)
+            third_priority.remove(applicant)
     for applicant in third_priority:
         if len(final_list[applicant[4]]) < N:
             final_list[applicant[4]].append(applicant)
     for k in final_list:
-        print(k)
-        for v in range(N):
+        print(k, len(applicants))
+        final_list[k] = sorted(final_list[k], key=lambda x: (-x[1], x[0]))
+        for v in range(len(final_list[k])):
             print(final_list[k][v][0], final_list[k][v][1])
+        print()
