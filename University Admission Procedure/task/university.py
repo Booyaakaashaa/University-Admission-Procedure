11ab285812d544276680aa73ae458b43ed102080
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-04-01 00:14:40 +0530

    Stage 4

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,11 +1,12 @@
 N = int(input())
-M = int(input())
-applicants = []
-for _ in range(N):
-    first_name, last_name, gpa = input().split(" ")
-    applicants.append([first_name + " " + last_name, float(gpa)])
 
-print("Successful applicants:")
+applicants = []
+with open("applicants.txt", 'r') as data:
+    for _ in range(N):
+        first_name, last_name, gpa, ch1, ch2, ch3 = data.readline().split(" ")
+        applicants.append([first_name + " " + last_name, float(gpa), ch1, ch2, ch3])
+    print(applicants)
+"""print("Successful applicants:")
 success_applicants = sorted(applicants, key=lambda x: (-x[1], x[0]))
 for applicant in success_applicants[:M]:
-    print(applicant[0])
+    print(applicant[0])"""
