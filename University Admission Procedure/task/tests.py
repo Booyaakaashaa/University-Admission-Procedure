ebf29ec64116bb3583f7f14eb2587313bcb0996a
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-04-01 19:07:30 +0530

    Stage 5

--- University Admission Procedure/task/tests.py
+++ University Admission Procedure/task/tests.py
@@ -17,14 +17,15 @@ class TestAdmissionProcedure(StageTest):
         ]
 
     @staticmethod
-    def sort_by_priority(applicants, priority_n, departments_names, departments_lists, max_students):
+    def sort_by_priority(applicants, priority_n, departments_names, departments_lists, max_students, exams):
         accepted_students = []
         for n, dep in enumerate(departments_names):
             dep_list = departments_lists[n]
+            exam_n = exams[n]
             if len(dep_list) == max_students:
                 continue
             students_needed = max_students - len(dep_list)
-            dep_applicants = [applicant[:2]
+            dep_applicants = [[applicant[0]] + [applicant[exam_n + 1]]
                               for applicant in applicants if applicant[-1][priority_n] == dep]
             dep_applicants = sorted(dep_applicants, key=lambda x: (-x[1], x[0]))[:students_needed]
             departments_lists[n].extend(dep_applicants)
@@ -35,18 +36,21 @@ class TestAdmissionProcedure(StageTest):
     @staticmethod
     def get_admission_lists(max_students):
         applicants = application_list.strip().split('\n')
-        departments = sorted(['Mathematics', 'Physics', 'Biotech', 'Chemistry', 'Engineering'])
+        departments = {'Mathematics': 2, 'Physics': 0, 'Biotech': 1, 'Chemistry': 1, 'Engineering': 3}
+        exams = [departments[dep] for dep in sorted(departments)]
+        departments = sorted(departments)
         applicants_data = []
         for line in applicants:
             line = line.split()
-            line = [line[0] + ' ' + line[1]] + [float(line[2])] + [line[3:]]
+            line = [line[0] + ' ' + line[1]] + [float(element) for element in line[2:-3]] + [line[-3:]]
             applicants_data.append(line)
         departments_lists = [[] for _ in departments]
         for i in range(len(applicants_data[-1][-1])):
-            applicants_data, departments_lists = TestAdmissionProcedure.sort_by_priority(applicants_data,
-                                                                                         i, departments,
+            applicants_data, departments_lists = TestAdmissionProcedure.sort_by_priority(applicants_data, i,
+                                                                                         departments,
                                                                                          departments_lists,
-                                                                                         max_students)
+                                                                                         max_students,
+                                                                                         exams)
         departments_lists = [[' '.join([str(el) for el in applicant])
                               for applicant in sorted(dep, key=lambda x: (-x[1], x[0]))]
                              for dep in departments_lists]
@@ -76,18 +80,39 @@ class TestAdmissionProcedure(StageTest):
             correct_applicants = admission_lists[i]
             output_applicants, output = output[:len(correct_applicants)], output[len(correct_applicants):]
             for j, applicant in enumerate(correct_applicants):
-                output_applicant = output_applicants[j]
-                if applicant.lower().strip() not in output_applicant.lower():
+                applicant_name, applicant_surname, score = applicant.split()
+                applicant_name = "{0} {1}".format(applicant_name, applicant_surname)
+                score = int(float(score))
+                output_applicant = output_applicants[j].strip().split(' ')
+                if len(output_applicant) != 3:
                     raise WrongAnswer("Line {0} for the {1} department "
-                                      "is supposed to contain the following applicant:\n"
-                                      "\"{2}\"\n"
-                                      "However, this line contains the following data:\n"
-                                      "\"{3}\"\n"
-                                      "Make sure the procedure of application is "
-                                      "implemented correctly in your program.".format(j + 1,
-                                                                                      department_name,
-                                                                                      applicant,
-                                                                                      output_applicant))
+                                      "does not seem to contain three elements: first name, last name and score.\n"
+                                      "Make sure you separate them "
+                                      "with one whitespace character.".format(j + 1,
+                                                                              department_name))
+                output_applicant_name = "{0} {1}".format(output_applicant[0], output_applicant[1])
+                try:
+                    output_score = int(float(output_applicant[-1]))
+                except ValueError:
+                    raise WrongAnswer("The second element in line {0} for the {1} department\n"
+                                      "does not seem to be a number: \"{2}\". \n"
+                                      "Make sure you format the output "
+                                      "as stated in the example.".format(j + 1,
+                                                                         department_name,
+                                                                         output_applicant[1]))
+                if applicant_name.lower().strip() not in output_applicant_name.lower():
+                    raise WrongAnswer("The first element in line {0} for the {1} department\n"
+                                      "does not seem to contain the correct name of the student ({2}).\n"
+                                      "Instead, it is equal to \"{3}\"".format(j + 1, department_name,
+                                                                               applicant_name,
+                                                                               output_applicant_name))
+
+                if score != output_score:
+                    raise WrongAnswer("The second element in line {0} for the {1} department\n"
+                                      "does not seem to contain the correct score of the student ({2}).\n"
+                                      "Instead, it is equal to \"{3}\"".format(j + 1, department_name,
+                                                                               score,
+                                                                               output_score))
 
         return CheckResult.correct()
 
