af8cb96423930d3c635ed513be87a65500502ba5
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-04-06 04:27:45 +0530

    Stage 6

--- University Admission Procedure/task/university.py
+++ University Admission Procedure/task/university.py
@@ -1,3 +1,6 @@
+import os
+
+
 def choice_num(subject):
     if subject == "Physics":
         return 1
@@ -18,24 +21,27 @@ with open("applicants.txt", 'r') as data:
     for line in data:
         first_name, last_name, phy, chem, math, cse, ch1, ch2, ch3 = line.strip().split(" ")
         applicants.append([first_name + " " + last_name, float(int(phy) / 2 + int(math) / 2), int(chem), int(math), float(int(cse) / 2 + int(math) / 2), float(int(phy) / 2 + int(chem) / 2), ch1, ch2, ch3])
-    first_priority = sorted(applicants, key=lambda x: (x[5], -x[choice_num(x[5])], x[0]))
-    second_priority = sorted(applicants, key=lambda x: (x[6], -x[choice_num(x[6])], x[0]))
-    third_priority = sorted(applicants, key=lambda x: (x[7], -x[choice_num(x[7])], x[0]))
+    first_priority = sorted(applicants, key=lambda x: (x[6], -x[choice_num(x[6])], x[0]))
+    second_priority = sorted(applicants, key=lambda x: (x[7], -x[choice_num(x[7])], x[0]))
+    third_priority = sorted(applicants, key=lambda x: (x[8], -x[choice_num(x[8])], x[0]))
     for applicant in first_priority:
-        if len(final_list[applicant[5]]) < N:
-            final_list[applicant[5]].append(applicant)
+        if len(final_list[applicant[6]]) < N:
+            final_list[applicant[6]].append(applicant)
             second_priority.remove(applicant)
             third_priority.remove(applicant)
     for applicant in second_priority:
-        if len(final_list[applicant[6]]) < N:
-            final_list[applicant[6]].append(applicant)
-            third_priority.remove(applicant)
-    for applicant in third_priority:
         if len(final_list[applicant[7]]) < N:
             final_list[applicant[7]].append(applicant)
+            third_priority.remove(applicant)
+    for applicant in third_priority:
+        if len(final_list[applicant[8]]) < N:
+            final_list[applicant[8]].append(applicant)
+    cwd = os.getcwd()
     for k in final_list:
-        print(k)
-        final_list[k] = sorted(final_list[k], key=lambda x: (-x[choice_num(k)], x[0]))
-        for v in range(len(final_list[k])):
-            print(final_list[k][v][0], final_list[k][v][choice_num(k)])
+        os.chdir(cwd)
+        with open(f"{k}.txt", "w") as out_file:
+            print(k)
+            final_list[k] = sorted(final_list[k], key=lambda x: (-x[choice_num(k)], x[0]))
+            for v in range(len(final_list[k])):
+                print(final_list[k][v][0], final_list[k][v][choice_num(k)], file=out_file)
         print()
