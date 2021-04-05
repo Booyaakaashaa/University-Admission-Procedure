4d8d6f63025f2f32ee9bedb6da4afaa5c5e21da7
Booyaakaashaa <2020pis5657@mnit.ac.in>
2021-04-06 04:11:15 +0530

    Stage 6

--- University Admission Procedure/task/tests.py
+++ University Admission Procedure/task/tests.py
@@ -1,7 +1,7 @@
 from hstest import *
 from test.application_list import application_list as application_list
 
-input_1 = ["1"]
+input_1 = ["5"]
 input_2 = ["23"]
 input_3 = ["10"]
 input_4 = ["15"]
@@ -18,14 +18,18 @@ class TestAdmissionProcedure(StageTest):
 
     @staticmethod
     def sort_by_priority(applicants, priority_n, departments_names, departments_lists, max_students, exams):
+
+        def mean(numbers):
+            return sum(numbers) / len(numbers)
+
         accepted_students = []
         for n, dep in enumerate(departments_names):
             dep_list = departments_lists[n]
-            exam_n = exams[n]
+            exam_numbers = exams[n]
             if len(dep_list) == max_students:
                 continue
             students_needed = max_students - len(dep_list)
-            dep_applicants = [[applicant[0]] + [applicant[exam_n + 1]]
+            dep_applicants = [[applicant[0]] + [mean([applicant[exam_n + 1] for exam_n in exam_numbers])]
                               for applicant in applicants if applicant[-1][priority_n] == dep]
             dep_applicants = sorted(dep_applicants, key=lambda x: (-x[1], x[0]))[:students_needed]
             departments_lists[n].extend(dep_applicants)
@@ -36,7 +40,9 @@ class TestAdmissionProcedure(StageTest):
     @staticmethod
     def get_admission_lists(max_students):
         applicants = application_list.strip().split('\n')
-        departments = {'Mathematics': 2, 'Physics': 0, 'Biotech': 1, 'Chemistry': 1, 'Engineering': 3}
+        departments = {'Mathematics': [2], 'Physics': [0, 2],
+                       'Biotech': [1, 0], 'Chemistry': [1],
+                       'Engineering': [3, 2]}
         exams = [departments[dep] for dep in sorted(departments)]
         departments = sorted(departments)
         applicants_data = []
@@ -46,11 +52,10 @@ class TestAdmissionProcedure(StageTest):
             applicants_data.append(line)
         departments_lists = [[] for _ in departments]
         for i in range(len(applicants_data[-1][-1])):
-            applicants_data, departments_lists = TestAdmissionProcedure.sort_by_priority(applicants_data, i,
-                                                                                         departments,
+            applicants_data, departments_lists = TestAdmissionProcedure.sort_by_priority(applicants_data,
+                                                                                         i, departments,
                                                                                          departments_lists,
-                                                                                         max_students,
-                                                                                         exams)
+                                                                                         max_students, exams)
         departments_lists = [[' '.join([str(el) for el in applicant])
                               for applicant in sorted(dep, key=lambda x: (-x[1], x[0]))]
                              for dep in departments_lists]
@@ -58,31 +63,32 @@ class TestAdmissionProcedure(StageTest):
 
     def check(self, reply: str, attach: list):
         n = int(attach[0])
-        output = reply.strip().split('\n')
-        output = [line for line in output if line]
-        if not output:
-            raise WrongAnswer("Your output seems to be empty.")
         department_names, admission_lists = self.get_admission_lists(n)
-        correct_n_lines = len(department_names) + sum([len(dep_list) for dep_list in admission_lists])
-        if len(output) != correct_n_lines:
-            raise WrongAnswer("Your output is supposed to contain {0} lines with data (N={1}).\n"
-                              "However, {2} lines with data are found.".format(correct_n_lines, n, len(output)))
-
         for i, department_name in enumerate(department_names):
-            if department_name.lower() not in output[0].lower():
-                raise WrongAnswer("The current line is supposed to contain the name of the department \"{0}\".\n"
-                                  "However, this line contains the following data:\n"
-                                  "\"{1}\"\n"
-                                  "Make sure that you output the department names. \n"
-                                  "Also, make sure that you output them "
-                                  "in correct order.\n".format(department_name, output[0]))
-            output = output[1:]
+            filename = department_name.lower() + ".txt"
+            try:
+                with open(filename, "r", encoding="utf-8") as fh:
+                    output_applicants = fh.read()
+            except FileNotFoundError:
+                raise WrongAnswer("The file {0} is not found.\n"
+                                  "Please make sure that you output results to files\n"
+                                  "and specify the correct paths for them.".format(filename))
+
+            if not output_applicants:
+                raise WrongAnswer("The file for the {0} department is empty.".format(department_name))
+
+            output_applicants = output_applicants.strip().split('\n')
+            output_applicants = [line for line in output_applicants if line]
             correct_applicants = admission_lists[i]
-            output_applicants, output = output[:len(correct_applicants)], output[len(correct_applicants):]
+            if len(correct_applicants) != len(output_applicants):
+                raise WrongAnswer("The file for the {0} department is expected to contain {1} line(s).\n"
+                                  "However, {2} lines are found.".format(department_name, len(correct_applicants),
+                                                                         len(output_applicants)))
+
             for j, applicant in enumerate(correct_applicants):
                 applicant_name, applicant_surname, score = applicant.split()
                 applicant_name = "{0} {1}".format(applicant_name, applicant_surname)
-                score = int(float(score))
+                score = round(float(score), 2)
                 output_applicant = output_applicants[j].strip().split(' ')
                 if len(output_applicant) != 3:
                     raise WrongAnswer("Line {0} for the {1} department "
@@ -92,7 +98,7 @@ class TestAdmissionProcedure(StageTest):
                                                                               department_name))
                 output_applicant_name = "{0} {1}".format(output_applicant[0], output_applicant[1])
                 try:
-                    output_score = int(float(output_applicant[-1]))
+                    output_score = round(float(output_applicant[-1]), 2)
                 except ValueError:
                     raise WrongAnswer("The second element in line {0} for the {1} department\n"
                                       "does not seem to be a number: \"{2}\". \n"
